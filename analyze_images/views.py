44c89eddec938e5e2850b0b584da774e78f8d454
yuyaebean <yuyaebean@gmail.com>
2020-08-18 22:35:42 +0900

    [Feat] Input image and response

--- analyze_images/views.py
+++ analyze_images/views.py
@@ -1,7 +1,18 @@
 from django.http import HttpResponse
+from .forms import UploadFileForm
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
 
+@csrf_exempt
 def index(request):
-    return HttpResponse("Hello World")
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        print(request.FILES)
+        if form.is_valid():
+            return HttpResponse(request.FILES['image'])
+        else:
+            return HttpResponse('Form is not valid')
+    else:
+        return HttpResponse(status=404)
