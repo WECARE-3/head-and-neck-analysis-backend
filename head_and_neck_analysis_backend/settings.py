3da6877c0951c9d317abe195d7c364fcf7c58121
yuyaebean <yuyaebean@gmail.com>
2020-08-25 21:08:10 +0900

    [Feat] Set CORS config

--- head_and_neck_analysis_backend/settings.py
+++ head_and_neck_analysis_backend/settings.py
@@ -27,11 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
 INSTALLED_APPS = [
     'analyze_images.apps.AnalyzeImagesConfig',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,6 +43,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
